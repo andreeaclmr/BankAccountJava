108afa0cea7b4811f210131e7862eefd4a9d4fe0
Andreea <andreea.celmare@gmail.com>
2024-11-05 10:36:19 +0100

    feat: start AccountTest

--- src/test/java/org/example/AccountTest.java
+++ src/test/java/org/example/AccountTest.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    @Test
+    @DisplayName("Create Account")
+    void test1() {
 
+    }
 }
